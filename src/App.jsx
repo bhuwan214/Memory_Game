cd0080c115725ac29b042f3f52eb03ea400ccd96
void-the-droid <bhuwan626282@gmail.com>
2024-11-22 15:30:46 +0545

    level modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import FetchPokemons from "./FetchPokemon"
+import LevelModal from "./components/LevelModal"
+
 export default function App() {
   
   const { pokemons, getRandomPokemons, shufflePokemons, setPokemons}=FetchPokemons()
 console.log(pokemons)
-
+return(
+  <>
+    <LevelModal/>
+  </>
+)
 
 
 }
