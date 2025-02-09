5102d8d57869ddde842e352357edf42834c1148e
void-the-droid <bhuwan626282@gmail.com>
2025-02-09 17:08:13 +0545

    new path

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import FetchPokemons from "./FetchPokemon"
-import LevelModal from "./components/LevelModal"
+import LevelModal from "./components/LevelSelectModal"
 
 export default function App() {
-  
-  const { pokemons, getRandomPokemons, shufflePokemons, setPokemons}=FetchPokemons()
-console.log(pokemons)
+
 return(
   <>
     <LevelModal/>
